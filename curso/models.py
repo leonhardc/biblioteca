2162f4b76e3a98c7a3fe62fb3e4808f6a27651fd
Leonardo <leonardo99692370costa@gmail.com>
2025-02-04 20:00:17 -0300

    APP UPDATE: Atualização nos models de curso e de usuario

--- curso/models.py
+++ curso/models.py
@@ -6,10 +6,12 @@ class Curso(models.Model):
         ('V', 'Vespertino'),
         ('I', 'Integral'),
     )
-    cod_curso = models.CharField(max_length=4, blank=False, null=False, verbose_name='Código do Curso')
+    cod_curso = models.CharField(unique=True,max_length=4, blank=False, null=False, verbose_name='Código do Curso')
     curso = models.CharField(max_length=25, blank=False, null=False, verbose_name='Curso')
     descricao = models.TextField(max_length=200, blank=True, null=False, verbose_name='Descrição do Curso')
     turno = models.CharField(max_length=1, choices=TURNOS, blank=False, null=False, verbose_name='Turno', default='M')
+    duracao = models.IntegerField(default=5,blank=False, null=False, verbose_name='Duração')
+
     class Meta:
         verbose_name = 'Curso'  
         verbose_name_plural = 'Cursos'
--- usuario/models.py
+++ usuario/models.py
@@ -6,12 +6,12 @@ import datetime
 
 class Aluno(models.Model):
     usuario = models.ForeignKey(User, verbose_name='Usuário', on_delete=models.DO_NOTHING)
-    matricula = models.CharField(max_length=6, blank=False, null=False, verbose_name='Matricula')
-    curso = models.CharField(max_length=25, blank=False, null=False, verbose_name='Curso')
+    matricula = models.CharField(unique=True,max_length=6, blank=False, null=False, verbose_name='Matricula')
+    curso = models.ForeignKey(Curso, on_delete=models.DO_NOTHING, blank=False, null=False, verbose_name='Curso')
     endereco = models.CharField(max_length=255, blank=False, null=False, verbose_name='Endereço')
-    cpf = models.CharField(max_length=11, blank=False, null=False, verbose_name='CPF')
+    cpf = models.CharField(unique=True,max_length=11, blank=False, null=False, verbose_name='CPF')
     ingresso = models.DateField(default=datetime.date.today, null=False, blank=False, verbose_name='Data de ingresso')
-    conclusao_prevista = models.DateField()
+    conclusao_prevista = models.DateField(default=datetime.date.today,blank=False, null=False, verbose_name='Conclusão')
 
     def __str__(self):
         return f'<{self.usuario}>'
@@ -29,7 +29,7 @@ class Professor(models.Model):
     )
     usuario = models.ForeignKey(User, verbose_name='Usuário', on_delete=models.DO_NOTHING)
     matricula = models.CharField(max_length=4, blank=False, null=False, verbose_name='Matricula')
-    curso = models.CharField(max_length=25, blank=False, null=False, verbose_name='Curso')
+    curso = models.ForeignKey(Curso, on_delete=models.DO_NOTHING, blank=False, null=False, verbose_name='Curso')
     cpf = models.CharField(max_length=11, blank=False, null=False, verbose_name='CPF')
     regime = models.CharField(max_length=3, blank=False, null=False, choices=JORNADA, verbose_name='Regime de trabalho')
     contratacao = models.DateField(default=datetime.date.today, blank=False, null=False, verbose_name='Data de Contratação')
