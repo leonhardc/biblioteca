be1c76793bc45b84921dd629c43a1c1d698dd261
Leonardo <leonardo99692370costa@gmail.com>
2025-02-13 15:46:19 -0300

    PROJECT UPDATE - Adicionado arquivo urls.py para o app livro

---
+++ livro/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+from livro.views import *
+
+app_name = "livro"
+
+urlpatterns = [
+    # Livros
+    path("listar-livros/",view=listar_livros, name="listar_livros"),
+    path("criar-livro/",view=criar_livro, name="criar_livro"),
+    path("detalhar-livro/<int:id_livro>/",view=detalhar_livro, name="detalhar_livro"),
+    path("atualizar-livro/<int:id_livro>/",view=atualizar_livro, name="atualizar_livro"),
+    path("deletar-livro/<int:id_livro>/",view=deletar_livro, name="deletar_livro"),
+    path("reservar-livro/<int:id_livro>/",view=reservar_livro, name="reservar_livro"),
+    path("emprestar-livro/<int:id_livro>/",view=emprestar_livro, name="emprestar_livro"),
+    # Autores
+    path("listar-autores/",view=listar_autores, name="listar_autores"),
+    path("criar-autor/",view=criar_autor, name="criar_autor"),
+    path("detalhar-autor/<int:id_autor>/",view=detalhar_autor, name="detalhar_autor"),
+    path("atualizar-autor/<int:id_autor>/",view=atualizar_autor, name="atualizar_autor"),
+    path("deletar-autor/<int:id_autor>/",view=deletar_autor, name="deletar_autor"),
+    # Categorias
+    path("listar-categorias/",view=listar_categorias, name="listar_categorias"),
+    path("criar-categoria/",view=criar_categoria, name="criar_categoria"),
+    path("detalhar-categoria/<int:id_categoria>/",view=detalhar_categoria, name="detalhar_categoria"),
+    path("atualizar-categoria/<int:id_categoria>/",view=atualizar_categoria, name="atualizar_categoria"),
+    path("deletar-categoria/<int:id_categoria>/",view=deletar_categoria, name="deletar_categoria"),
+]
