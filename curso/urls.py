5ab702a737e185a619fa7891735a133403e27601
Leonardo <leonardo99692370costa@gmail.com>
2025-02-24 16:32:23 -0300

    PROJECT UPDATE - Adicionado arquivo de urls do app de cursos

---
+++ curso/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from curso import views
+
+app_name = 'curso'
+urlpatterns = [
+    path('cursos/', views.index, name='index'),
+    path('criar-curso/', views.criar_curso, name='criar-curso'),
+    path('ler-curso/<int:cid>/', views.ler_curso, name='ler-curso'),
+    path('atualizar-curso/<int:cid>/', views.atualizar_curso, name='atualizar-curso/<int:cid>/'),
+    path('deletar-curso/<int:cid>/', views.deletar_curso, name='deletar-curso'),
+]
