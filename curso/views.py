8fd96eee9670a6319fb5eab4b3917c45824a5f3b
Leonardo <leonardo99692370costa@gmail.com>
2025-02-24 16:31:58 -0300

    PROJECT UPDATE - Adicionado esqueleto das views do app curso

--- curso/views.py
+++ curso/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+# CRUD de curso
+def index(request):
+    pass
+
+def criar_curso(request):
+    pass
+
+def ler_curso(request, cid):
+    # Página de detalhes de cada curso
+    pass
+
+def atualizar_curso(request, cid):
+    pass
+
+def deletar_curso(request, cid):
+    pass
+
