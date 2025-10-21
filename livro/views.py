63db30a3c13f176688e4ab5e0664e771ed8ca9f1
Leonardo <leonardo99692370costa@gmail.com>
2025-10-21 11:45:28 -0300

    Implementada Paginação na view de livros e no template de livros

--- livro/views.py
+++ livro/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpRequest
+from django.core.paginator import Paginator
+from .models import Livro
 
 # Método de Listar Livros, Autores e Categorias
-def listar_livros(request):
-    pass
+def listar_livros(request: HttpRequest) -> HttpResponse:
+    template_name = "livro/livros.html"
+    livros = Livro.objects.all()
+    paginator = Paginator(livros, 20)
+    page_number = request.GET.get("page")
+    livros = paginator.get_page(page_number)
+    return render(request, template_name=template_name, context={"livros": livros})
 
 def listar_autores(resquest):
     pass
