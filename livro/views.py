eb77167e342011c05d4f4486b32ae24c62cf0d5d
Leonardo <leonardo99692370costa@gmail.com>
2025-10-21 15:58:41 -0300

    Implementado sistema de pesquisa mas falta o problema de bug com a paginacao

--- livro/views.py
+++ livro/views.py
@@ -2,15 +2,32 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from django.core.paginator import Paginator
 from .models import Livro
+from django.db.models import Q
 
 # Método de Listar Livros, Autores e Categorias
 def listar_livros(request: HttpRequest) -> HttpResponse:
     template_name = "livro/livros.html"
-    livros = Livro.objects.all()
-    paginator = Paginator(livros, 20)
-    page_number = request.GET.get("page")
-    livros = paginator.get_page(page_number)
-    return render(request, template_name=template_name, context={"livros": livros})
+    itens_por_pagina = 20
+    termo_pesquisa = request.GET.get("pesquisa", "")
+    if termo_pesquisa:
+        pesquisa = Livro.objects.filter(
+            Q(titulo__icontains=termo_pesquisa)|
+            Q(subtitulo__icontains=termo_pesquisa)|
+            Q(autores__nome__icontains=termo_pesquisa)).distinct()
+        if len(pesquisa) < itens_por_pagina:
+            itens_por_pagina = len(pesquisa)
+            if itens_por_pagina == 0:
+                itens_por_pagina = 1
+        paginator = Paginator(pesquisa, itens_por_pagina)
+        page_number = request.GET.get("page")
+        livros = paginator.get_page(page_number)
+        return render(request, template_name=template_name, context={"livros": livros})
+    else:
+        livros = Livro.objects.all()
+        paginator = Paginator(livros, itens_por_pagina)
+        page_number = request.GET.get("page")
+        livros = paginator.get_page(page_number)
+        return render(request, template_name=template_name, context={"livros": livros})
 
 def listar_autores(resquest):
     pass
