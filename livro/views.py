8ef3fb75af62cc834355bf2d6854997174f16058
Leonardo <leonardo99692370costa@gmail.com>
2025-10-23 09:37:24 -0300

    Inserido esqueleto das views e tipagens de parametros

--- livro/views.py
+++ livro/views.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator
 from .models import Livro
 from django.db.models import Q
 
+
 # Método de Listar Livros, Autores e Categorias
 def listar_livros(request: HttpRequest) -> HttpResponse:
     template_name = "livro/livros.html"
@@ -18,8 +19,8 @@ def listar_livros(request: HttpRequest) -> HttpResponse:
             itens_por_pagina = len(pesquisa)
             if itens_por_pagina == 0:
                 itens_por_pagina = 1
-        paginator = Paginator(pesquisa, itens_por_pagina)
         page_number = request.GET.get("page")
+        paginator = Paginator(pesquisa, itens_por_pagina)
         livros = paginator.get_page(page_number)
         return render(request, template_name=template_name, context={"livros": livros, 'termo_busca': termo_pesquisa})
     else:
@@ -29,54 +30,54 @@ def listar_livros(request: HttpRequest) -> HttpResponse:
         livros = paginator.get_page(page_number)
         return render(request, template_name=template_name, context={"livros": livros})
 
-def listar_autores(resquest):
-    pass
+def listar_autores(resquest: HttpRequest) -> HttpResponse:
+    return HttpResponse('View listar autores')
 
-def listar_categorias(request):
-    pass
+def listar_categorias(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("View listar Categorias")
 
 # Implementação do CRUD para livro
-def criar_livro(request):
-    pass
+def criar_livro(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("View Criar Livro")
 
-def detalhar_livro(request, id_livro):
-    pass
+def detalhar_livro(request: HttpRequest, id_livro:int) -> HttpResponse:
+    return HttpResponse("View detalhar Livro")
 
-def atualizar_livro(request, id_livro):
-    pass
+def atualizar_livro(request:HttpRequest, id_livro:int) -> HttpResponse:
+    return HttpResponse("View Atualizar Livro")
 
-def deletar_livro(request, id_livro):
-    pass
+def deletar_livro(request:HttpRequest, id_livro:int) -> HttpResponse:
+    return HttpResponse("View Deletar Livro")
 
 # Implementação de Emprestimo e Reserva de Livros
-def reservar_livro(request, id_livro, usuario):
-    pass
+def reservar_livro(request:HttpRequest, id_livro:int, usuario:str) -> HttpResponse:
+    return HttpResponse("View reservar livro")
 
-def emprestar_livro(request, id_livro, usuario):
-    pass
+def emprestar_livro(request:HttpResponse, id_livro:int, usuario:str) -> HttpResponse:
+    return HttpResponse("View Emprestar Livro")
 
 # CRUD para Autor
-def criar_autor(request):
-    pass 
+def criar_autor(request:HttpRequest) -> HttpResponse:
+    return HttpResponse("View criar Autor")
 
-def detalhar_autor(request, id_autor):
-    pass 
+def detalhar_autor(request:HttpRequest, id_autor:int) -> HttpResponse:
+    return HttpResponse("View detalhar Autor")
 
-def atualizar_autor(request, id_autor):
-    pass
+def atualizar_autor(request:HttpRequest, id_autor:int) -> HttpResponse:
+    return HttpResponse("View atualizar Autor")
 
-def deletar_autor(request, id_autor):
-    pass
+def deletar_autor(request:HttpRequest, id_autor:int) -> HttpResponse:
+    return HttpResponse("View deletar Autor")
 
 # CRUD para Categoria
-def criar_categoria(request):
-    pass
+def criar_categoria(request:HttpRequest) -> HttpResponse:
+    return HttpResponse("View criar Categoria")
 
-def detalhar_categoria(request, id_categoria):
-    pass
+def detalhar_categoria(request:HttpRequest, id_categoria:int) -> HttpResponse:
+    return HttpResponse("View detalhar Categoria")
 
-def atualizar_categoria(request, id_categoria):
-    pass
+def atualizar_categoria(request:HttpRequest, id_categoria:int) -> HttpResponse:
+    return HttpResponse("View atualizar Categoria")
 
-def deletar_categoria(request, id_categoria):
-    pass
+def deletar_categoria(request:HttpRequest, id_categoria:int) -> HttpResponse:
+    return HttpResponse("View deletar Categoria")
