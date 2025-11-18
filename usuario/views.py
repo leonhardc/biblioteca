667e688d47bd3f5c39c5f05d9fd5d3b829881131
Leonardo <leonardo99692370costa@gmail.com>
2025-11-18 15:56:23 -0300

    Primeira implementação das views de usuario/aluno

--- usuario/views.py
+++ usuario/views.py
@@ -1,3 +1,10 @@
+# O que deve ser implementado aqui:
+# TODO: 1. Todas as views de Usuarios. Elas tem que ser Intermediarias a todos as operações
+# Que envolvam usuarios. 
+# TODO: 2. Tem que ser Implementado o Sistema de Login Redirecionando cada um dos tipos de usuarios
+# as suas respectivas paginas iniciais
+# TODO: 3. Tem que ser implementado todo um sistema de controle de acesso a determinadas views. Onde cada
+# tipo de usuario so pode acessar as views as quais ele tem permissao
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpRequest
 from usuario.forms import LoginForm, FormularioAluno
@@ -9,7 +16,7 @@ from usuario.models import Aluno, Professor, Funcionario
 # from curso.models import Curso
 # from django.core.paginator import Paginator
 from utils.utils import *
-
+from utils.formularios.utils_forms import informacoes_formulario_aluno
 # TODO: Essa view só pode ser acessada se o usuário estiver logado
 def index(request:HttpRequest) -> HttpResponse:
     template = 'usuario/index.html'
@@ -75,24 +82,14 @@ def sair(request:HttpRequest):
 
 # CRUD de Aluno
 def listar_alunos(request:HttpRequest):
-    query_alunos = Aluno.objects.all()
-    return render(request, 'lista_alunos.html', context={'alunos':query_alunos})
-
-
-def criar_aluno(request:HttpRequest):
-    if request.method == 'GET':
-        formulario_aluno = FormularioAluno()
-        template_name = 'formulario-cadastro-aluno.html'
-        return render(request, template_name, context={'form':formulario_aluno})
-    else: 
-        if request.method == 'POST':
-            # TODO: Salvar as informações de usuário e aluno do formulário no banco de dados
-            formulario_novo_aluno = FormularioAluno(request.POST)
-            # TODO: Implementar página de retorno com mensagem de sucesso
-            if formulario_novo_aluno.is_valid():
-                return HttpResponse('OK')
-            else:
-                return render(request, template_name='formulario-cadastro-aluno.html', context={'form':formulario_novo_aluno})
+    template_name = 'usuario/aluno/listar_alunos.html'
+    return render(
+        request, 
+        template_name, 
+        context={
+            'alunos':Aluno.objects.all()
+            }
+        )
 
 def dashaboard_aluno(request:HttpRequest):
     if request.method == "GET":
@@ -102,22 +99,76 @@ def dashaboard_aluno(request:HttpRequest):
         }
         return render(request, template_name, context=contexto)
 
+def criar_aluno(request:HttpRequest):
+    if request.method == 'GET':
+        formulario_aluno = FormularioAluno()
+        template_name = 'formulario-cadastro-aluno.html'
+        return render(request, template_name, context={'form':formulario_aluno})
+    if request.method == 'POST':
+            pass
+
 def ler_aluno(request:HttpRequest, uid:int):
-    pass
+    # TODO: O usuario do tipo aluno e usuario do tipo funcionario e administrador tem acesso a essa view
+    # TODO: Criar o template abaixo
+    template_name = "usuario/aluno/ler_aluno.html"
+    if Aluno.objects.filter(id=uid).exists():
+        aluno = Aluno.objects.get(id=uid)
+        return render(request, template_name, context={'aluno':aluno})
+    else: 
+        url_anterior = request.META.get('HTTP_REFERER')
+        messages.add_message(request, messages.ERROR, 'Aluno não encontrado.')
+        return redirect(url_anterior)
 
 def atualizar_aluno(request:HttpRequest, uid:int):
-    pass
+    # TODO: Essa view so pode ser acessada por funcionarios, administradores e pelo aluno
+    # TODO: Criar o template abaixo
+    template_name = 'usuario/aluno/atualizar_dados_aluno.html'
+    if request.method == "GET":
+        if Aluno.objects.filter(id=uid).exists():
+            aluno = Aluno.objects.get(id=uid)
+            endereco = separar_endereco(aluno.endereco) 
+            data_aluno = informacoes_formulario_aluno(aluno, endereco)
+            formulario = FormularioAluno(initial=data_aluno)
+            return render(request, template_name, context={'aluno':aluno, 'form':formulario})
+        else:
+            messages.add_message(request, messages.ERROR, 'Aluno não encontrado.')
+            url_anterior = request.META.get('HTTP_REFERER')
+            return redirect(url_anterior)
+    if request.method == "POST":
+        # TODO: Salvar os dados do aluno no banco de dados
+        pass
 
 def deletar_aluno(request:HttpRequest, uid:int):
-    pass
+    # TODO: Somente acessivel pelo usuario administrador
+    if Aluno.objects.filter(id=uid).exists() and request.user.is_authenticated:
+        Aluno.objects.get(id=uid).delete()
+        return HttpResponse("Aluno Deletado com Sucesso.")
+    else: 
+        url_anterior = request.META.get('HTTP_REFERER')
+        messages.add_message(request, messages.ERROR, 'Aluno não encontrado')
+        return redirect(url_anterior)
 
-# TODO: Mudar as views de detalhes para o app de administrador
 def detalhes_aluno(request:HttpRequest, uid:int):
-    if request.method == 'GET':
+    if request.method == 'GET' and request.user.is_authenticated:
         template_name = 'admin/dashboard_admin_detalhes_usuarios.html'
-        aluno = Aluno.objects.get(id=uid)
-        contexto = {'aluno': aluno}
-        return render(request, template_name, context=contexto)
+        usuario = request.user
+        if user_is_aluno(usuario) or user_is_funcionario(usuario) or request.user.is_staff: 
+            return render(
+                request, 
+                template_name, 
+                context={
+                    'aluno': Aluno.objects.get(id=uid)
+                    }
+                )
+        else: 
+            url_anterior = request.META.get('HTTP_REFERER')
+            messages.add_message(request, messages.ERROR, 'Usuario não tem permissão para acessar essa pagina')
+            return redirect(url_anterior)
+    else:
+        url_anterior = request.META.get('HTTP_REFERER')
+        messages.add_message(request, messages.ERROR, 'Usuario não autenticado')
+        return redirect(url_anterior)
+
 
 # CRUD de Professor
 def criar_professor(request:HttpRequest):
