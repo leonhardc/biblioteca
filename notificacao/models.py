d2e881bebe0be16c427150bc6902172b44fef110
Leonardo <leonardo99692370costa@gmail.com>
2026-02-06 12:38:39 -0300

    Adicionado model do app notificacao

--- notificacao/models.py
+++ notificacao/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Notificacao(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='notificacoes'
+    )
+    title = models.CharField(max_length=100)
+    message = models.TextField()
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
