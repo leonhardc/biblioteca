9bedba586a0f8d0893528b424b3a4b83a316195a
Leonardo <leonardo99692370costa@gmail.com>
2026-02-06 12:39:01 -0300

    Registrado na pagina de administrador

--- notificacao/admin.py
+++ notificacao/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Notificacao
+
+@admin.register(Notificacao)
+class NotificacaoAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'is_read', 'created_at')
+    list_filter = ('is_read',)
