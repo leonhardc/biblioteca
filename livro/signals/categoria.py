22ed567325499b2f83f169312809ddfce105c192
Leonardo <leonardo99692370costa@gmail.com>
2026-02-11 11:10:08 -0300

    Signals de categorias criado

---
+++ livro/signals/categoria.py
@@ -0,0 +1,43 @@
+import logging
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+from livro.models import Categoria
+from notificacao.models import Notificacao, NotificationType
+
+logger = logging.getLogger('livro')
+
+@receiver(post_save, sender=Categoria)
+def categoria_criada(sender, instance, created, **kwargs):
+    if not created:
+        return
+
+    logger.info(
+        'Categoria criada | id=%s | nome="%s"',
+        instance.id,
+        instance.nome
+    )
+
+@receiver(post_save, sender=Categoria)
+def categoria_atualizada(sender, instance, created, **kwargs):
+    if created:
+        return
+
+    logger.info(
+        'Categoria atualizada | id=%s | nome="%s"',
+        instance.id,
+        instance.nome
+    )
+
+@receiver(pre_save, sender=Categoria)
+def categoria_excluida(sender, instance, **kwargs):
+    if not instance.pk:
+        return
+
+    antigo = Categoria.objects.get(pk=instance.pk)
+
+    if antigo.ativo and not instance.ativo:
+        logger.info(
+            'Categoria excluída | id=%s | nome="%s"',
+            instance.id,
+            instance.nome
+        )
